5f27db50153442e4293336a7f3ff89dbaaad644a
Gurukiran <guru.sode@gmail.com>
2020-03-16 13:05:05 +0530

    Create slider

--- src/components/Flights.js
+++ src/components/Flights.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
+import { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 class Flights extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {}
+        this.onChangeSlider = this.onChangeSlider.bind(this);
     }
 
-    render() { 
-        console.log(this.props.location.state)
-        return ( 
-            <h1>Flights</h1>
-         );
+    onChangeSlider(value){
+        console.log(value)
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <div className="col s2">
+                    <Range defaultValue={[2000, 10000]} onChange={this.onChangeSlider} allowCross={false} min={2000} max={10000}/>
+                </div>
+                <div className="col s10">
+
+                </div>
+            </div>
+        );
     }
 }
- 
+
 export default Flights;
